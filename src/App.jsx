841d4b7201c2bf7137f77b3e3f7108755e7bf1bd
gabriele-fontana <gabrielefontana.2001@gmail.com>
2026-03-18 23:03:18 +0100

    add Bootstrap for styling; refactor Buttons component to use Bootstrap classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 
-
-
-import Buttons from './components/Buttons'
-import AppHeader from './components/AppHeader'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Buttons from './components/Buttons';
+import AppHeader from './components/AppHeader';
 function App() {
 
  
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,12 +3,15 @@ import  {languages} from '../assets/languages'
 
 export default function Buttons() {
 
+    
     return (
-languages.map((language)=>{
-    return (
-    <button>{language.title}</button>
+    <div className='d-flex justify-content-around'>
+        {languages.map((language)=>{
+        return (
+            <button className='btn btn-primary '>{language.title}</button>
+        )})}
+               
+    </div>
     )
-})
-        
-    )
+    
 }
